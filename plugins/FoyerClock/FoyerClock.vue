5841a0a73754a8b901ec24af1ee0532dcbdc0dc9
kiniamaro <vdyotte@gmail.com>
2018-03-07 20:23:23 -0500

    Clock: add toggle for year and seconds

--- plugins/FoyerClock/FoyerClock.vue
+++ plugins/FoyerClock/FoyerClock.vue
@@ -1,14 +1,28 @@
 <template>
   <div id="FoyerClock">
-    <div class="dateContainer">
+    <div class="dateContainer" v-show="!edit">
       <div>
         <span class="is-size-1 is-primary">{{hours}}</span>
         <span class="is-size-1 has-text-grey">{{minutes}}</span>
-        <span class="is-size-5 has-text-grey-dark">{{seconds}}</span>
+        <span class="is-size-5 has-text-grey-dark" v-show="config.seconds">{{seconds}}</span>
       </div>
       <div class="has-text-grey-light">
-        {{day}}, {{month}} {{date}}, {{year}}
+        {{day}}, {{month}} {{date}}
+        <span v-show="config.year">, {{year}}</span>
       </div>
+    </div>
+    <div v-show="edit">
+      <div class="control">
+        <label class="checkbox">
+          <input v-model="config.seconds" type="checkbox">
+          Show seconds
+        </label>
+        <label class="checkbox">
+          <input v-model="config.year" type="checkbox">
+          Show Year
+        </label>
+      </div>
+</div>
     </div>
   </div>
 </template>
@@ -31,7 +45,26 @@ export default {
         lineNumbers: true,
         line: true,
       },
-      config: {}
+      config: {
+        seconds: false,
+        year: true
+      }
+    }
+  },
+  methods: {
+    setConfig () {
+      this.$config.setConfig(this.name, this.config);
+    }
+  },
+  watch: {
+    "config.seconds": function (val) {
+      this.setConfig();
+    },
+    "config.year": function (val) {
+      this.setConfig();
+    },
+    editing: function (val) {
+      this.edit = val;
     }
   },
   computed: {
@@ -48,13 +81,13 @@ export default {
       return this.now.toLocaleString('en-us', {  weekday: 'long' });
     },
     hours: function () {
-      return this.now.getHours();
+      return `${this.now.getHours()}`.padStart(2, "0");
     },
     minutes: function () {
-      return this.now.getMinutes();
+      return `${this.now.getMinutes()}`.padStart(2, "0");;
     },
     seconds: function () {
-      return this.now.getSeconds();
+      return `${this.now.getSeconds()}`.padStart(2, "0");;
     }
   },
   created () {
